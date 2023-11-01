771767f02f40bb604e8e5e74199f822e2916fb18
chrisdecarie <134742901+chrisdecarie@users.noreply.github.com>
2023-11-01 14:46:00 -0400

    Update myobject.php
    Edited/removed file for correction.

--- myobject.php
+++ myobject.php
@@ -1,28 +1,2 @@
-<?php
 
-class Sunglasses {
-    private $wearable;
-    private $foldable = 0;
-    public $isActive = false;
 
-    public function __construct() {
-        $this->wearable = "not set";
-    }
-    private function getWearable() {
-        return $this->wearable;
-    }
-    private function setWearable($newValue) {
-        $this->wearable = $newValue;
-    }
-    public function ask() {
-        return "wearable: " . $this->getWearable() . " foldable: " . $this->foldable;
-    }
-    public function action($data) {
-        $this->setWearable($data);
-    }
-}
-
-$testObject1 = new Sunglasses();
-// $testObject1->action(123);
-echo $testObject1->ask();
-echo $testObject1->isActive ? "active" : "not-active";
