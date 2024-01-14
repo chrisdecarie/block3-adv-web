8a7b8fe4ce849a9a5b9d4709af73348a64d02b2a
chrisdecarie <134742901+chrisdecarie@users.noreply.github.com>
2024-01-13 19:16:16 -0500

    Update main-controllers.php
    Updated main-controllers.php

--- project001/controllers/main-controllers.php
+++ project001/controllers/main-controllers.php
@@ -1,42 +1,42 @@
 <?php
 // root folder>controllers>main-controller.php
 
-require_once 'product-type-controller.php';
-require_once 'product-category-controller.php'; 
-require_once 'product-brand-controller.php';
-require_once 'product-controller.php';
-require_once 'models/product-type-model.php';
-require_once 'models/product-category-model.php'; 
-require_once 'models/product-brand-model.php';
-require_once 'models/product-model.php';
-require_once 'views/product-type-view.php';
-require_once 'views/product-category-view.php'; 
-require_once 'views/product-brand-view.php';
-require_once 'views/product-view.php';
+require_once 'pizza-controller.php';
+require_once 'pizza-ingredients-controller.php'; 
+require_once 'pizza-size-controller.php';
+require_once 'pizza-orders-controller.php';
+require_once 'models/pizza-model.php';
+require_once 'models/pizza-ingredients-model.php'; 
+require_once 'models/pizza-size-model.php';
+require_once 'models/pizza-orders-model.php';
+require_once 'views/pizza-view.php';
+require_once 'views/pizza-ingredients-view.php'; 
+require_once 'views/pizza-size-view.php';
+require_once 'views/pizza-orders-view.php';
 
 // Create instances of models and views
-$productTypeModel = new ProductTypeModel($link);
-$productCategoryModel = new ProductCategoryModel($link);
-$productBrandModel = new ProductBrandModel($link);
-$productModel = new ProductModel($link);
+$pizzaModel = new PizzaModel($link);
+$pizzaIngredientsModel = new PizzaIngredientsModel($link);
+$pizzaSizeModel = new PizzaSizeModel($link);
+$pizzaOrdersModel = new PizzaOrdersModel($link);
 
-$productTypeView = new ProductTypeView();
-$productCategoryView = new ProductCategoryView();
-$productBrandView = new ProductBrandView();
-$productView = new ProductView();
+$pizzaView = new PizzaView();
+$pizzaIngredientsView = new PizzaIngredientsView();
+$pizzaSizeView = new PizzaSizeView();
+$pizzaOrdersView = new PizzaOrdersView();
 
 
 // Create instances of controllers
-$productTypeController = new ProductTypeController($productTypeModel, $productTypeView);
-$productCategoryController = new ProductCategoryController($productCategoryModel, $productCategoryView);
-$productBrandController = new ProductBrandController($productBrandModel, $productBrandView);
-$productController = new ProductController($productModel, $productView);
+$pizzaController = new PizzaController($pizzaModel, $pizzaView);
+$pizzaIngredientsController = new PizzaIngredientsController($pizzaIngredientsModel, $pizzaIngredientsView);
+$pizzaSizeController = new PizzaSizeController($pizzaSizeModel, $pizzaSizeView);
+$pizzaOrdersController = new PizzaOrdersController($pizzaOrdersModel, $pizzaOrdersView);
 
 
 // Handle the requests
-$productTypeController->handleTypeRequest();
-$productCategoryController->handleCategoryRequest();
-$productBrandController->handleBrandRequest();
-$productController->handleProductRequest();
+$pizzaController->handleTypeRequest();
+$pizzaIngredientsController->handleCategoryRequest();
+$pizzaSizeController->handleBrandRequest();
+$pizzaOrdersController->handleProductRequest();
 
 ?>
